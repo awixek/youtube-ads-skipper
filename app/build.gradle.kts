2b9e848f4b362dc50444ba47c3c3709308a38ff4
Abhishek kumar <yadavramnaresh435@gmail.com>
2026-09-13 00:51:07 +0530

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.autoskip.youtube"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.autoskip.youtube"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1"
+    }
+}
